6e1ac114b195a79d41890bc310cf2cc1aad74cdc
Aleksander2006 <aleksvansanten@gmail.com>
2024-11-26 15:45:17 +0100

    Simpel werkend autotje

---
+++ Assets/Carmovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Carmovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    public void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, rotation, 0);
+    }
+}
