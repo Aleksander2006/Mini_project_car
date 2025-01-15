9b6cd7c69b75898507d2a6494bddc2400220bed3
Aleksander2006 <aleksvansanten@gmail.com>
2025-01-15 21:05:59 +0100

    de wielen sturen nu ook mee. Movement aangepast

--- Assets/Scripts/Carmovement.cs
+++ Assets/Scripts/Carmovement.cs
@@ -1,27 +1,22 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Carmovement : MonoBehaviour
 {
-    public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
     public GameObject lights;
+    public Transform wheelmeshFL;
+    public Transform wheelmeshRL;
+
+    float translation = 0f;
+    float speedModifier = 30;
+    float speed = 80f;
 
     private bool isHighBeamOn = false;
 
     public void Update()
     {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-
-        // Move translation along the object's z-axis
-        transform.Translate(0, 0, translation);
-
-        // Rotate around our y-axis
-        transform.Rotate(0, rotation, 0);
-
+        Steering();
         Lights_highbeam();
     }
 
@@ -38,5 +33,37 @@ public class Carmovement : MonoBehaviour
                 
             } 
         }  
+    }
+
+    void Steering (){
+        if (Input.GetKeyDown(KeyCode.W)){
+            translation = speedModifier += 10; // 10 km steeds harder
+            Debug.Log(speedModifier);
+            if(speedModifier >= 100){ //limit voor snelheid (110km)
+                speedModifier = 100;
+            }
+        }
+        if (Input.GetKey(KeyCode.S)){
+            translation = speedModifier -= 10;
+            if(speedModifier <= 0){
+                speedModifier = 0;
+            }
+        }
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation * Time.deltaTime); // Pas de positie aan
+        
+        float steerAngle = Input.GetAxis("Horizontal") * rotationSpeed;
+        float rotation = steerAngle;
+
+        //Dit zorgt ervoor dat de steering angle wordt gelimiteerd.
+        steerAngle = Mathf.Clamp(steerAngle, -5f, 5f);
+
+        wheelmeshRL.transform.localRotation = Quaternion.Euler(-180, steerAngle, 0);
+        wheelmeshFL.transform.localRotation = Quaternion.Euler(-0.433f, steerAngle, 0);
+        
+        rotation *= Time.deltaTime;
+
+        // Rotate around our y-axis
+        transform.Rotate(0, rotation, 0);
     }
 }
